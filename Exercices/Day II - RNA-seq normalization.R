11f20e4014ce152d73f8ec14c904ea9bdcf5c3b6
athieffry <axel.thieffry@gmail.com>
2023-11-16 09:33:26 +0100

    update

---
+++ Exercices/Day II - RNA-seq normalization.R
@@ -0,0 +1,102 @@
+# RNA-seq normalization exercises: Solutions
+# Axel Thieffry - Nov. 2023
+library(tidyverse)
+library(tidylog)
+library(magrittr)
+library(readxl)
+library(reshape2)
+library(patchwork)
+library(ggrepel)
+'%!in%' <- Negate('%in%')
+'h' <- head
+'at' <- as_tibble
+'l' <- length
+
+PLOT <- FALSE
+
+
+# 1. Read expression matrix
+# -------------------------
+mat <- read_xlsx('~/Dropbox/Documents/HEH - Guest Lecturing/HEH_ATH_2023/datasets/gene_count_matrix.xlsx')
+dim(mat)
+
+
+# 2. Gene expression density plot per sample, colored by condition
+# ----------------------------------------------------------------
+mat_filtered <- mat_filtered %>%
+            melt(id.vars='gene_id', variable.name='sample', value.name='count') %>%
+            mutate('condition'=str_remove(sample, '_R.*$'))
+
+if(PLOT) {
+          ggplot(mat_long, aes(x=count, col=condition, groups=sample)) +
+                 geom_density()
+
+          ggplot(mat_long, aes(x=count, colour=condition, groups=sample)) +
+                 geom_density() +
+                 scale_x_log10()
+
+          ggplot(mat_filtered, aes(x=count+1, col=condition, groups=sample)) +
+                geom_density() +
+                scale_x_log10() +
+                theme(aspect.ratio=1) +
+                labs(title='Gene counts distribution',
+                     subtitle='pseudocount: 1',
+                     x='count (log-scaled, pseudocount=1)')
+          }
+
+
+# 3. Removing lowly expressed genes (based on readcount)
+# ------------------------------------------------------
+genes_tokeep <- mat %>%
+                melt(id.vars='gene_id', variable.name='sample', value.name='counts') %>%
+                mutate('pass'=counts >= 10) %>%
+                separate('sample', c('condition', 'rep'), '_') %>%
+                group_by(gene_id, condition) %>%
+                summarize('sumpass'=sum(pass)) %>%
+                filter(sumpass == 3) %>%
+                ungroup() %>%
+                pull('gene_id') %>%
+                unique()
+
+mat_filtered <- filter(mat, gene_id %in% genes_tokeep)
+
+
+# 4. RPM normalization
+# --------------------
+total_reads <- mat %>% select(-gene_id) %>% colSums()
+
+rpm_filtered <- mat_filtered %>%
+                column_to_rownames('gene_id') %>%
+                apply(1, \(x) divide_by(x, total_reads/1e6)) %>%
+                t() %>%
+                as.data.frame()
+
+# sanity check - should be close to 1M
+colSums(rpm_filtered)
+
+
+# 5. Compare distributions
+# ------------------------
+if(PLOT) {
+          gg_a <- mat_filtered %>%
+                  melt(id.vars='gene_id', variable.name='sample', value.name='count') %>%
+                  mutate('condition'=str_remove(sample, '_R.*$')) %>%
+                  ggplot(aes(x=count+1, col=condition, groups=sample)) +
+                         geom_density() +
+                         scale_x_log10() +
+                         theme(aspect.ratio=.5) +
+                         labs(title='Counts distribution', subtitle='filtered data', x='log(count+1)')
+
+          gg_b <- rpm_filtered %>%
+                  rownames_to_column('gene_id') %>%
+                  melt(id.vars='gene_id', variable.name='sample', value.name='rpm') %>%
+                  mutate('condition'=str_remove(sample, '_R.*$')) %>%
+                  ggplot(aes(x=rpm+1, col=condition, groups=sample)) +
+                         geom_density() +
+                         scale_x_log10() +
+                         theme(aspect.ratio=.5) +
+                         labs(title='RPM distribution', subtitle='filtered data', x='log(count+1)')
+
+          gg_a / gg_b
+          }
+
---
+++ Exercices/Day III - RNA-seq RPKM normalization.md
@@ -0,0 +1,8 @@
+## RNA-seq RPKM - exercises
+
+Download the Arabidopsis thaliana TAIR10 genome annotation in BED format found in the dataset folder.
+
+1. In one step, read the file in R and give the dataframe (or tibble) the appropriate column names (chr, start, end, gene_id, score, strand).
+2. Check the number of gene annotations and verify that there are no duplicated genes.
+3. Create a new column named "size" that computed the length of genes.
+4. Check how many genes from our RPM normalized matrix are found in the BED annotation.
